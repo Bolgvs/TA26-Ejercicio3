1f18407128b1a78d6f147b7322f0aee8401cf65c
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-08-24 01:06:22 +0200

    commit fix imports

--- src/main/java/controller/ProductosController.java
+++ src/main/java/controller/ProductosController.java
@@ -12,9 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import dto.Cajeros;
 import dto.Productos;
-import service.CajerosServiceImpl;
 import service.ProductosServiceImpl;
 
 @RestController
